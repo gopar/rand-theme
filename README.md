0d056a27060e4a026a5aaf2a5f3cba604d00fbbe
Daniel Gopar <gopardaniel@yahoo.com>
2015-10-25 12:18:19 -0700

    Ability to iterate through list. Update README

--- README.md
+++ README.md
@@ -7,11 +7,30 @@ This package randomly selects a theme at start-up, if you like to use different
 themes then this is for you! \o/
 
 ## Features
+ - Randomly pick a theme from a list you want.
+ You can setup a list that you only want to choose from or
+ create a list of themes you *never* want to use.
+
+- You iterate through the themes you have, in case you don't
+want it to be random.
 
 ## How to install
 
 ## How to use
 
-## TODO:
+Before using you **must** first setup **either** `rand-theme-unwanted` or `rand-theme-wanted`. These  are lists in which the package will use
+to decide what themes to select.
 
-- *Maybe* add a timer for randomly changing theme?
+Example:
+```lisp
+(require 'rand-theme)
+;; Themes I never want to be selected
+(setq rand-theme-unwanted '(leuven tango adwaita light-blue tsdh-light dichromacy whiteboard))
+(rand-theme)
+```
+```lisp
+(require 'rand-theme)
+;; Themes I *only* want to be selected
+(setq rand-theme-wanted '(abyss lush manoj-dark))
+(rand-theme)
+```
--- rand-theme.el
+++ rand-theme.el
@@ -7,8 +7,7 @@
 ;; Created: Tue Oct 20 22:21:57 2015 (-0700)
 ;; Version: 0.1
 ;; Package-Requires: ()
-;; URL:
-;; Doc URL:
+;; URL: https://github.com/gopar/rand-theme
 ;; Keywords:
 ;; Compatibility:
 ;;
@@ -16,11 +15,6 @@
 ;;
 ;;; Commentary:
 ;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Change Log:
 ;;
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -42,35 +36,69 @@
 ;;
 ;;; Code:
 
+(require 'cl)
+
 (defcustom rand-theme-unwanted nil
   "List of themes that you *don't* want to randomly select."
   :type 'sexp
   :group 'theme-list)
 
 (defcustom rand-theme-wanted nil
-  "List of themes that you *only* want to randomly select.  If this is
-non-nil then it will have a higher precedence than `rand-theme-unwanted'."
+  "List of themes that you *only* want to randomly select.
+If this is non-nil then it will have a higher precedence than `rand-theme-unwanted'."
   :type 'sexp
   :group 'theme-list)
 
-(defun rand-theme ()
-  "Randomly pick a theme from `rand-theme-unwanted' or if non-nil from `rand-theme-wanted'.
-Will raise error if both of these variables are nil."
-  (interactive)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helper functions
+(defun rand-theme--get-theme-list ()
+  "Return a list of themes to use"
   (if (null (or rand-theme-unwanted rand-theme-wanted))
       (error "Neither `rand-theme-unwanted' nor `rand-theme-wanted' have been set."))
   (let ((available-themes (custom-available-themes)) (theme nil))
     (if (null rand-theme-wanted)
         ;; Filter out unwanted themes
-        (mapcar '(lambda (unwanted) (setq available-themes (remove unwanted available-themes))) rand-theme-unwanted)
+        (mapc '(lambda (unwanted) (setq available-themes (remove unwanted available-themes))) rand-theme-unwanted)
       ;; No need to filter since we already have a list we want to use
       (setq available-themes rand-theme-wanted))
+    ;; return themes to use
+    available-themes))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Interactive functions
+
+(defun rand-theme ()
+  "Randomly pick a theme from `rand-theme-unwanted' or if non-nil from `rand-theme-wanted'.
+Will raise error if both of these variables are nil."
+  (interactive)
+  (let ((available-themes (rand-theme--get-theme-list))
+        (theme nil))
     ;; Randomly choose a theme
     (setq theme (nth (random (length available-themes)) available-themes))
     ;; Disable ALL themes
     (mapcar 'disable-theme custom-enabled-themes)
     (load-theme theme t)))
 
+(defun rand-theme-iterate ()
+  "Iterate through the list of themes.
+In case you want to go incremental."
+  (interactive)
+  (let ((available-themes (rand-theme--get-theme-list))
+        (curr-pos nil)
+        ;; current enabled theme. Only get first one. Ignore others
+        (curr-theme (nth 0 (mapcar 'symbol-name custom-enabled-themes))))
+    ;; Get the next theme in line
+    (setq curr-pos (1+ (cl-position (intern curr-theme) available-themes)))
+    (if (>= curr-pos (length available-themes))
+        (setq curr-pos 0))
+    (setq curr-theme (nth curr-pos available-themes))
+    ;; Disable ALL themes
+    (mapcar 'disable-theme custom-enabled-themes)
+    (load-theme curr-theme)
+    (message "Loaded Theme: %s" (symbol-name curr-theme))
+    ))
+
+(provide 'rand-theme)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
